ec38c783ba22377183d78f2a8cd6a655f5f15414
nourhene amdouni <nourhene803@gmail.com>
2025-02-01 16:24:08 +0100

    login and sinup

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [    
-     { path: '', component: LoginComponent },  
+export const routes: Routes = [     
+     { path: '', component: HomeComponent },  
+     { path: 'login', component: LoginComponent },  
      { path: 'signup', component: SignUpComponent }, 
 
 
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
